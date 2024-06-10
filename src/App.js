1ee0e186f5df1b6f5c019f7d859bd26b6292721c
Elshuye2112 <basie.elsh2112@gmail.com>
2024-06-10 16:09:25 +0300

    Crud application using react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Create from './components/Create';
+import Read from './components/Read';
+import Update from './components/Update';
+
+import {  Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+      <div className='main'>
+     
+       <Routes>
+        
+       <Route  path='/' element={<Create/>} />
+        
+       
+        <Route  path='/read' element={<Read/>} />
+      
+
+      <Route path='/update' element={<Update/>} />
+       </Routes>
+
+      
+        </div>
+  
   );
 }
 
 export default App;
+
+
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+
+
+
+function Home(){
+    <h1>Welcome to these page</h1>
+}
+
+export default Home;
